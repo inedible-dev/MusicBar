6e3fd37450b65631d4beca65ba414e0e0a1f957c
Wongkraiwich Chuenchomphu <work.kentakoong@gmail.com>
2022-11-16 19:27:02 +0700

    Initial Commit

---
+++ MusicBar/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MusicBar
+//
+//  Created by Wongkraiwich Chuenchomphu on 11/16/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
