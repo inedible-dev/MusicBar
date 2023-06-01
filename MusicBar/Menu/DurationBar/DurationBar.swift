9060a184f68939813858fff5729a486581aa2ddb
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2023-06-01 15:56:16 +0700

    add: durationbar for menu

---
+++ MusicBar/Menu/DurationBar/DurationBar.swift
@@ -0,0 +1,26 @@
+//
+//  DurationBar.swift
+//  MusicBar
+//
+//  Created by Wongkraiwich Chuenchomphu on 1/6/23.
+//
+
+import SwiftUI
+
+struct DurationBar: View {
+    var value: Double
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
+                    .opacity(0.3)
+                    .foregroundColor(.white)
+                
+                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                    .foregroundColor(.white)
+                    .animation(.linear(duration: 0.2))
+            }.cornerRadius(45.0)
+        }
+    }
+}
