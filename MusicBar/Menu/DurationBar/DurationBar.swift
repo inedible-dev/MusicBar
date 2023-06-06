1b0848aeab5c3dd4027d7e6066c3a08d25d16566
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2023-06-06 15:37:17 +0700

    updpate: durationbar code formatting

--- MusicBar/Menu/DurationBar/DurationBar.swift
+++ MusicBar/Menu/DurationBar/DurationBar.swift
@@ -16,7 +16,6 @@ struct DurationBar: View {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
                     .opacity(0.3)
                     .foregroundColor(.white)
-                
                 Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
                     .foregroundColor(.white)
                     .animation(.linear(duration: 0.2))
