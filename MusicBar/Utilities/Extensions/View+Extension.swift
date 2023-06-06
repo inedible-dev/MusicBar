64ed82e0549dc82c4ab8d11b41859a30590b09de
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2023-06-06 15:39:10 +0700

    add: rect corner viewextension

--- MusicBar/Utilities/Extensions/View+Extension.swift
+++ MusicBar/Utilities/Extensions/View+Extension.swift
@@ -5,10 +5,63 @@
 //  Created by Wongkraiwich Chuenchomphu on 23/5/23.
 //
 
-import Foundation
-
 import SwiftUI
 import Combine
+
+struct RectCorner: OptionSet {
+    
+    let rawValue: Int
+    
+    static let topLeft = RectCorner(rawValue: 1 << 0)
+    static let topRight = RectCorner(rawValue: 1 << 1)
+    static let bottomRight = RectCorner(rawValue: 1 << 2)
+    static let bottomLeft = RectCorner(rawValue: 1 << 3)
+    
+    static let allCorners: RectCorner = [.topLeft, topRight, .bottomLeft, .bottomRight]
+}
+
+struct RoundedCornersShape: Shape {
+    
+    var radius: CGFloat = .zero
+    var corners: RectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let p1 = CGPoint(x: rect.minX, y: corners.contains(.topLeft) ? rect.minY + radius  : rect.minY )
+        let p2 = CGPoint(x: corners.contains(.topLeft) ? rect.minX + radius : rect.minX, y: rect.minY )
+        
+        let p3 = CGPoint(x: corners.contains(.topRight) ? rect.maxX - radius : rect.maxX, y: rect.minY )
+        let p4 = CGPoint(x: rect.maxX, y: corners.contains(.topRight) ? rect.minY + radius  : rect.minY )
+        
+        let p5 = CGPoint(x: rect.maxX, y: corners.contains(.bottomRight) ? rect.maxY - radius : rect.maxY )
+        let p6 = CGPoint(x: corners.contains(.bottomRight) ? rect.maxX - radius : rect.maxX, y: rect.maxY )
+        
+        let p7 = CGPoint(x: corners.contains(.bottomLeft) ? rect.minX + radius : rect.minX, y: rect.maxY )
+        let p8 = CGPoint(x: rect.minX, y: corners.contains(.bottomLeft) ? rect.maxY - radius : rect.maxY )
+        
+        
+        path.move(to: p1)
+        path.addArc(tangent1End: CGPoint(x: rect.minX, y: rect.minY),
+                    tangent2End: p2,
+                    radius: radius)
+        path.addLine(to: p3)
+        path.addArc(tangent1End: CGPoint(x: rect.maxX, y: rect.minY),
+                    tangent2End: p4,
+                    radius: radius)
+        path.addLine(to: p5)
+        path.addArc(tangent1End: CGPoint(x: rect.maxX, y: rect.maxY),
+                    tangent2End: p6,
+                    radius: radius)
+        path.addLine(to: p7)
+        path.addArc(tangent1End: CGPoint(x: rect.minX, y: rect.maxY),
+                    tangent2End: p8,
+                    radius: radius)
+        path.closeSubpath()
+        
+        return path
+    }
+}
 
 extension View {
     /// A backwards compatible wrapper for iOS 14 `onChange`
@@ -16,5 +69,9 @@ extension View {
         self.onReceive(Just(value)) { (value) in
             onChange(value)
         }
+    }
+    
+    func roundedCorners(radius: CGFloat, corners: RectCorner) -> some View {
+        clipShape(RoundedCornersShape(radius: radius, corners: corners))
     }
 }
