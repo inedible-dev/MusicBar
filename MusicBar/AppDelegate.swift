ef03969cf2db6204534071fec234200982c622ad
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2023-06-06 22:07:56 +0700

    cleanup: appdelegate

--- MusicBar/AppDelegate.swift
+++ MusicBar/AppDelegate.swift
@@ -22,20 +22,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             UserDefaults.standard.set(false, forKey: "isLaunchedFirstTime")
         }
         
-         let statusBar = StatusBar()
+        let statusBar = StatusBar()
         
         statusBar.startTimer()
         statusBar.setupMenu()
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
+    
+    
 }
 
