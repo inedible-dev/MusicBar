43ca0dc06d87ca036ea9a9c955d9d23878503e8f
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2023-12-11 15:47:44 +0700

    update: general settings fields

--- MusicBar/Views/SettingsView/Tabs/General/GeneralSettingsView.swift
+++ MusicBar/Views/SettingsView/Tabs/General/GeneralSettingsView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 import LaunchAtLogin
 
+@available(macOS 12.0, *)
 struct GeneralSettingsView: View {
+    
+    @AppStorage("songTitleOnly") var songTitleOnly = false
+    @AppStorage("limitText") var limitText = false
+    @AppStorage("maxStatusBarCharacters") var maxStatusBarCharacters = 40
+    
     var body: some View {
         VStack {
             LaunchAtLogin.Toggle {
                 Text("Launch at login")
             }
+            Toggle(isOn: $songTitleOnly) {
+                Text("Song Title Only")
+            }.onChange(of: songTitleOnly) { _ in
+                StatusBar.setMedia()
+            }
+            HStack {
+                Text("Max Status Bar Characters")
+                TextField("", value: $maxStatusBarCharacters.max(70), format: .number)
+                    .frame(maxWidth: 40)
+                Stepper("", value: $maxStatusBarCharacters, in: 0...70)
+                    .onChange(of: maxStatusBarCharacters) { _ in
+                        StatusBar.setMedia()
+                    }
+            }
         }.padding()
     }
-}
-
-#Preview {
-    GeneralSettingsView()
 }
