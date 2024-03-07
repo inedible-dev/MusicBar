6e6e10dcc52e00056bdbf37fc1d4253649b47bab
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2024-03-07 10:34:08 +0700

    init: widgetkit

---
+++ MusicBarWidget/MusicBarWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MusicBarWidgetBundle.swift
+//  MusicBarWidget
+//
+//  Created by Wongkraiwich Chuenchomphu on 7/3/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MusicBarWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MusicBarWidget()
+    }
+}
