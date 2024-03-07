722a026d35eb5951dda46d685489fc22dbc5fc03
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2024-03-07 10:33:53 +0700

    fix: negative durationbar using abs

--- MusicBar/Views/MenuView/Views/DurationBar/DurationBar.swift
+++ MusicBar/Views/MenuView/Views/DurationBar/DurationBar.swift
@@ -16,7 +16,11 @@ struct DurationBar: View {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
                     .opacity(0.3)
                     .foregroundColor(.white)
-                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                Rectangle()
+                    .frame(
+                        width: abs(min(CGFloat(self.value) * geometry.size.width, geometry.size.width)),
+                        height: abs(geometry.size.height)
+                    )
                     .foregroundColor(.white)
                     .animation(.linear(duration: 0.2))
             }.cornerRadius(45.0)
